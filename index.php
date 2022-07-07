73df6c06fa9a986577f076196f4d3af568778a74
Kardast <dottorsf@gmail.com>
2022-07-07 17:24:16 +0200

    Include once dir

--- index.php
+++ index.php
@@ -1,49 +1,10 @@
 <?php
-class Movie {
-
-    static public $counter_of_objects = 0;
-
-    public $name;
-    public $genre;
-    public $nationality;
-    public $film_length;
-    public $director;
-    public $year;
-    public $quantity;
-    public $message = '';
-
-    public function __construct($c_name, $c_genre, $c_nationality, $c_film_length, $c_director, $c_year, $c_quantity)
-    {
-        $this->name = $c_name;
-        $this->genre = $c_genre;
-        $this->nationality = $c_nationality;
-        $this->film_length = $c_film_length;
-        $this->director = $c_director;
-        $this->year = $c_year;
-        $this->quantity = $c_quantity;
-
-        $this->movieAvailability();
-
-        self::$counter_of_objects++;
-    }
-
-    public function movieAvailability() {
-        if ($this->quantity < 1) {
-            $this->message = 'Non disponibile';
-        } else {
-            $this->message = 'Disponibile';
-        }
-    }
-
-}
-
+include_once __DIR__ . '/Movies.php';
 
 $scarface = new Movie('Scarface', 'Gangster', 'USA', '170 min', 'Brian De Palma', 1983, 10);
 $scarArray = (array) $scarface;
 $the_dark_knight = new Movie('The Dark Knight', 'Action', 'UK, USA', '152 min', 'Christopher Nolan', 2008, 0);
 $knightArray = (array) $the_dark_knight;
-
-
 
 // var_dump($scarface, $the_dark_knight);
 // var_dump($scarArray);
